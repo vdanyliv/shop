3606ef676e352a386bda95d6420b008c29a2c01c
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-23 16:23:15 +0300

    Added css loaders & plugins, setting up devServer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,16 +1,40 @@
-module.export = {
+var webpack = require("webpack");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
 	entry: './app/main.js',
 	output: {
-		path: './build',
-		filename: 'app.bundle.js',
+		path: './build/',
+		filename: 'app.bundle.js'
 	},
 	module: {
 		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            }
+        ]
+	},
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([
+            { from: './app/tpl', to: './tpl' },
+            { from: './app/index.html', to: './' }
+        ])
+    ],
+    devServer: {
+        contentBase: './build/',
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    }
 };
