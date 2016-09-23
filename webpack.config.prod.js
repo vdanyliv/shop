011d52db777ff428ae883ee7d392d8c85b71c82e
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-23 19:04:53 +0300

    setting up prod config

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,16 +1,36 @@
-module.export = {
-	entry: './app/main.js',
+var webpack = require("webpack");
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+	entry: {
+        app: ['./app/main.js']
+    },
 	output: {
-		path: './build',
-		filename: 'app.bundle.js',
+		path: __dirname + '/build',
+		filename: 'app.bundle.js'
 	},
 	module: {
 		loaders: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				loader: 'babel-loader'
-			}
-		]
-	}
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader"
+            },
+            {
+                test   : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+                loader : 'file-loader'
+            }
+        ]
+	},
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin([
+            { from: './app/tpl', to: './tpl' },
+            { from: './app/index.html', to: './' }
+        ])
+    ]
 };
