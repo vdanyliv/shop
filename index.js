c59ee70db7d42299ea902202fc0892103e3ededc
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-24 19:06:48 +0300

    CORS fix for dev env

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 let webpack = require('webpack');
 let webpackDevServer = require('webpack-dev-server');
-let webpackConfig = require('./webpack.config.prod');
+let webpackConfigProd = require('./webpack.config.prod');
+let webpackConfigDev = require('./webpack.config.dev');
 
 let express = require('express');
 let app = express();
 let bodyParser = require('body-parser');
 
-//proccess information
+//proccess env
 var env = process.env.USER_ENV;
 
 /*webpack*/
-webpackConfig.entry.app.unshift('webpack-dev-server/client?http://localhost:9000/', 'webpack/hot/dev-server');
-
-
 if (env === 'dev') {
-    let compiler = webpack(webpackConfig);
+    webpackConfigDev.entry.app.unshift('webpack-dev-server/client?http://localhost:9000/', 'webpack/hot/dev-server');
+    let compiler = webpack(webpackConfigDev);
     let jsServer = new webpackDevServer(compiler, {
         hot: true,
         contentBase: './build/',
@@ -24,30 +23,35 @@ if (env === 'dev') {
         }
     });
 
-    jsServer.listen(9000, 'localhost', function() {
+    jsServer.listen(9000, '0.0.0.0', function() {
         console.error('Front-end listen: localhost:9000');
     });
 }
 else {
-    webpack(webpackConfig, function() {
+    webpack(webpackConfigProd, function() {
         console.error('bundle created');
     });
 }
 
 /*express*/
+
+if (env === 'prod') app.use(express.static('./build/'));
+if (env === 'dev') {
+    app.use(function(req, res, next) {
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:9000');
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        next();
+    });
+}
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 
-let port = process.env.PORT || 9001;
+let port = process.env.PORT || env !== 'prod' ? 9001 : 9000;
 let apiModule = require('./server/api');
 
 app.use('/api', apiModule);
-
-app.all('/build/*', function (req, res) {
-    proxy.web(req, res, {
-        target: 'http://localhost:9000'
-    });
-});
 
 app.listen(port, '0.0.0.0', function() {
 	console.error('Back-end listen: localhost:' + port);
