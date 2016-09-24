17294f9d9b9db81293d80746b83428437289bdaa
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-24 19:02:26 +0300

    Finished API for /getProducts

--- server/api.js
+++ server/api.js
@@ -1,9 +1,17 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let fs = require('fs');
+let router = express.Router();
 
 
-router.get('/getProducts', function (req, res) {
-	res.send('hello world');
+router.post('/getProducts', (req, res) => {
+	let request = req.body;
+	let count = request.count;
+	let productList = fs.readFileSync(__dirname + '/mock/products.json', 'UTF8');
+	let parsedData = JSON.parse(productList);
+
+	if (count > parsedData.length) count = parsedData.length;
+
+	res.send(parsedData.slice(0, count));
 });
 
 
