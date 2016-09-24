28f838675b2e9fb044bb077ad3272df59e30e0e0
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-24 19:07:15 +0300

    add new deps | productApiService

--- app/main.js
+++ app/main.js
@@ -1,11 +1,14 @@
 require('./common/styles/bootstrap/css/bootstrap.min.css');
+require('./common/styles/custom.css');
 
 import angular from 'angular';
 import uirouter from 'angular-route';
 import routing from './main.routes';
+import productApiServise from './services/product.api.service';
 
 angular.module('app.shop', ['ngRoute'])
-	.config(routing);
+	.config(routing)
+	.factory('productApiServise', productApiServise);
 
 
 angular.bootstrap(document, ['app.shop']);
