ca773a94fd8f8fdbea858cad3b8ea6c9e0470e47
Volodymyr Danyliv <volodymyr@1worldonline.com>
2016-09-26 17:37:27 +0300

    Added uglify supporting, minor fixes

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -28,14 +28,29 @@ module.exports = {
 	},
     plugins: [
         new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    drop_console: true,
+                    unsafe: true
+                },
+                output: {
+                    comments: false
+                }
+        }),
         new CopyWebpackPlugin([
             { from: './app/tpl', to: './tpl' },
             { from: './app/index.html', to: './' },
             { from: './app/common', to: './common' }
         ]),
         new webpack.DefinePlugin({
-            API_URL: JSON.stringify('http://localhost:9000'),
-            IMG_URL: JSON.stringify('http://localhost:9000')
+            API_URL: JSON.stringify(process.env.DOMAIN ? process.env.DOMAIN : 'http://localhost:9000'),
+            IMG_URL: JSON.stringify(process.env.DOMAIN ? process.env.DOMAIN : 'http://localhost:9000')
         })
-    ]
+    ],
+    resolve: {
+        modulesDirectories: [
+          './node_modules'
+        ]        
+    }
 };
